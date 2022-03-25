8c96b38017764a686cd0672a23aff4e6f798c4f5
Daniel <ddoherty6@gmail.com>
2022-03-24 17:02:17 -0700

    implement client side in react

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="w-100 mt-auto bg-secondary p-4">
+      <div className="container">&copy; {Date()} by Dan Doherty</div>
+    </footer>
+  );
+};
+
+export default Footer;
